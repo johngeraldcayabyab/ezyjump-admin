7e6a7814894f14bf2cbdf3044dad71d64616d211
Dlareg <johngeraldcayabyab@gmail.com>
2023-09-12 12:23:10 +0800

    removed env

---
+++ bin/docker_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker-compose -f docker-compose.prod.yml exec app composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev --no-progress --no-plugins --no-scripts --no-ansi
+docker-compose -f docker-compose.prod.yml exec app npm install --production --omit=dev --prefer-offline --no-audit --progress=false
+docker-compose -f docker-compose.prod.yml exec app npm install laravel-mix@latest
+docker-compose -f docker-compose.prod.yml exec app npm install --save-dev vite laravel-vite-plugin
+docker-compose -f docker-compose.prod.yml exec app npm run build
+docker-compose -f docker-compose.prod.yml exec app composer dump-autoload -o
+
+docker-compose -f docker-compose.prod.yml exec app php artisan key:generate --no-interaction --force
+docker-compose -f docker-compose.prod.yml exec app php artisan migrate:fresh --seed --no-interaction --force
+docker-compose -f docker-compose.prod.yml exec app php artisan optimize:clear
+docker-compose -f docker-compose.prod.yml exec app php artisan storage:link
