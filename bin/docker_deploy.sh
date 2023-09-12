5d162345e6a984fb457943867add507795d6fa3e
Dlareg <johngeraldcayabyab@gmail.com>
2023-09-12 12:53:19 +0800

    added mysql command for reference

--- bin/docker_deploy.sh
+++ bin/docker_deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#CREATE USER 'laraveluser'@'%';
+#GRANT ALL PRIVILEGES ON laravel.* TO 'laraveluser'@'%';
+#SET PASSWORD FOR 'laraveluser'@'%' = 'testpass';
+
 docker compose -f docker-compose.prod.yml exec app composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev --no-progress --no-plugins --no-scripts --no-ansi
 docker compose -f docker-compose.prod.yml exec app npm install --production --omit=dev --prefer-offline --no-audit --progress=false
 docker compose -f docker-compose.prod.yml exec app npm install laravel-mix@latest
