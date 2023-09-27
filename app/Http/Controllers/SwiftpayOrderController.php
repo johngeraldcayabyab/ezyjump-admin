3d2c61d6d2353d0c68fc6668f4cd6869e8501ab7
Dlareg <johngeraldcayabyab@gmail.com>
2023-09-27 15:40:02 +0800

    revert controller

--- app/Http/Controllers/SwiftpayOrderController.php
+++ app/Http/Controllers/SwiftpayOrderController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Exception;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
@@ -14,43 +13,43 @@ class SwiftpayOrderController
     public function order(Request $request)
     {
         info($request->all());
-//        $token = env('SWIFTPAY_1_TOKEN');
-//        $bearerToken = "Bearer $token";
-//        $data = [
-//            'amount' => $request->amount,
-//            'customerDetails' => [
-//                'customerName' => (string)Str::uuid(),
-//                'country' => 'PH',
-////                'email' => 'email@example.com',
-////                'phone' => '09088764955',
-////                'city' => 'Quezon City',
-////                'state' => 'Metro Manila',
-////                'postcode' => '1110',
-////                'address1' => '39 Sarangaya Ave. Brgy. White Plains'
-//            ],
-//            'institutionCode' => 'GCASH',
-//            'callbackUrl' => 'https://redirect.me/goodstuff',
-//            'transactionId' => $request->transactionId
-//        ];
-//        info($data);
-//        try {
-//            $client = new Client([
-//                'base_uri' => 'https://api.ezyjump-pay.com'
-//            ]);
-//            $response = $client->post('/api/orders', [
-//                'headers' => [
-//                    'Authorization' => $bearerToken,
-//                    'Content-Type' => 'application/json'
-//                ],
-//                'json' => $data
-//            ]);
-//            $responseJson = json_decode($response->getBody(), true);
-//            info($responseJson);
-//            return $responseJson;
-//        } catch (Exception $exception) {
-//            $message = $exception->getMessage();
-//            Log::error($message);
-//            return ['status' => 'error', 'message' => $message];
-//        }
+        $token = env('SWIFTPAY_1_TOKEN');
+        $bearerToken = "Bearer $token";
+        $data = [
+            'amount' => $request->amount,
+            'customerDetails' => [
+                'customerName' => (string)Str::uuid(),
+                'country' => 'PH',
+//                'email' => 'email@example.com',
+//                'phone' => '09088764955',
+//                'city' => 'Quezon City',
+//                'state' => 'Metro Manila',
+//                'postcode' => '1110',
+//                'address1' => '39 Sarangaya Ave. Brgy. White Plains'
+            ],
+            'institutionCode' => 'GCASH',
+            'callbackUrl' => 'https://redirect.me/goodstuff',
+            'transactionId' => $request->transactionId
+        ];
+        info($data);
+        try {
+            $client = new Client([
+                'base_uri' => 'https://api.ezyjump-pay.com'
+            ]);
+            $response = $client->post('/api/orders', [
+                'headers' => [
+                    'Authorization' => $bearerToken,
+                    'Content-Type' => 'application/json'
+                ],
+                'json' => $data
+            ]);
+            $responseJson = json_decode($response->getBody(), true);
+            info($responseJson);
+            return $responseJson;
+        } catch (Exception $exception) {
+            $message = $exception->getMessage();
+            Log::error($message);
+            return ['status' => 'error', 'message' => $message];
+        }
     }
 }
