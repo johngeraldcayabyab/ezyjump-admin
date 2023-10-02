b25bb2260206d45d54b92babb9c7815606ca3bf4
Dlareg <johngeraldcayabyab@gmail.com>
2023-10-03 00:15:21 +0800

    create only one filter

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -84,6 +84,50 @@
                 <form
                     @submit.prevent="{loading, swiftpayOrders, meta} = await fetchSwiftpayOrders('{{route('swiftpay_query_orders.index')}}', {...search, ...getDateFromAndTo()})"
                 >
+                    <div class="flex mb-3">
+                        <label for="search-dropdown"
+                               class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"></label>
+                        <button
+                            id="dropdown-button"
+                            data-dropdown-toggle="dropdown"
+                            class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+                            type="button"
+                            x-html="search.label"
+                        >
+                        </button>
+                        <div id="dropdown"
+                             class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+                                <li>
+                                    <button
+                                        type="button"
+                                        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                        x-on:click="search = {field: 'transaction_id', label: 'Transaction Id'}"
+                                    >
+                                        Transaction Id
+                                    </button>
+                                </li>
+                                <li>
+                                    <button
+                                        type="button"
+                                        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                        x-on:click="search = {field: 'reference_number', label: 'Reference Number'}"
+                                    >
+                                        Reference Number
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="relative w-full">
+                            <input
+                                type="search"
+                                id="search-dropdown"
+                                class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                                placeholder="Search Transaction id, Reference number..."
+                                x-model="search.value"
+                            >
+                        </div>
+                    </div>
                     <div class="flex mb-3 items-center justify-between">
                         <div date-rangepicker class="flex items-center">
                             <div class="relative">
@@ -129,59 +173,7 @@
 
                 <form
                     @submit.prevent="{loading, swiftpayOrders, meta} = await fetchSwiftpayOrders('{{route('swiftpay_query_orders.index')}}', {...search, ...getDateFromAndTo()})">
-                    <div class="flex">
-                        <label for="search-dropdown"
-                               class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"></label>
-                        <button
-                            id="dropdown-button"
-                            data-dropdown-toggle="dropdown"
-                            class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
-                            type="button"
-                            x-html="search.label"
-                        >
-                        </button>
-                        <div id="dropdown"
-                             class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-                                <li>
-                                    <button
-                                        type="button"
-                                        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        x-on:click="search = {field: 'transaction_id', label: 'Transaction Id'}"
-                                    >
-                                        Transaction Id
-                                    </button>
-                                </li>
-                                <li>
-                                    <button
-                                        type="button"
-                                        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        x-on:click="search = {field: 'reference_number', label: 'Reference Number'}"
-                                    >
-                                        Reference Number
-                                    </button>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="relative w-full">
-                            <input
-                                type="search"
-                                id="search-dropdown"
-                                class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                                placeholder="Search Transaction id, Reference number..."
-                                x-model="search.value"
-                            >
-                            <button type="submit"
-                                    class="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-indigo-600 rounded-r-lg border bg-indigo-600 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
-                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                     viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                </svg>
-                                <span class="sr-only">Search</span>
-                            </button>
-                        </div>
-                    </div>
+
                 </form>
             </div>
         </div>
