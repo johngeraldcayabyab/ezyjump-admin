06e0705aa0b8fa0582828b3413deea331d4fa3c3
Dlareg <johngeraldcayabyab@gmail.com>
2023-10-10 12:20:03 +0800

    remove comments

--- app/Http/Controllers/SwiftpayQueryOrderController.php
+++ app/Http/Controllers/SwiftpayQueryOrderController.php
@@ -8,7 +8,6 @@ use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Str;
 
 class SwiftpayQueryOrderController extends Controller
@@ -64,56 +63,6 @@ class SwiftpayQueryOrderController extends Controller
 
     public function statistics(): JsonResponse
     {
-//        $yesterdayStart = Carbon::yesterday('Asia/Manila')->startOfDay();
-//        $yesterdayEnd = Carbon::yesterday('Asia/Manila')->endOfDay();
-
-//        $todayStart = Carbon::today('Asia/Manila')->startOfDay();
-//        $todayEnd = Carbon::today('Asia/Manila')->endOfDay();
-
-//        $start_of_day = Carbon::today('Asia/Manila');
-//        $end_of_day = Carbon::today('Asia/Manila')->endOfDay();
-
-//        $totalAmountYesterday = SwiftpayQueryOrder::whereBetween('created_at', [$yesterdayStart, $yesterdayEnd])->sum('amount');
-//        $totalAmountToday = SwiftpayQueryOrder::whereBetween('created_at', [$start_of_day->timezone('UTC')->toDateTimeString(), $end_of_day->timezone('UTC')->toDateTimeString()])->sum('amount');
-
-//        info(number_format($totalAmountYesterday));
-//        info(number_format($totalAmountToday));
-
-//        $date = Carbon::create(2023, 9, 14, 0, 0, 0, 'Asia/Manila');
-//
-//        $data = SwiftpayQueryOrder::whereDate(DB::raw('CONVERT_TZ(created_at, \'+00:00\', \'+08:00\')'), $date)->sum('amount');
-//        info($data);
-
-
-// Get yesterday's total amount
-//        $yesterday = Carbon::yesterday('Asia/Manila')->setTimezone('UTC');
-//        $yesterdayTotalAmount = SwiftpayQueryOrder::whereDate(DB::raw('CONVERT_TZ(created_at, \'+00:00\', \'+08:00\')'), $yesterday)->where('ORDER_STATUS', 'EXECUTED')->sum('amount');
-//
-//// Get today's total amount
-//        $today = Carbon::today('Asia/Manila')->setTimezone('UTC');
-//        $todayTotalAmount = SwiftpayQueryOrder::whereDate(DB::raw('CONVERT_TZ(created_at, \'+00:00\', \'+08:00\')'), $today)->where('ORDER_STATUS', 'EXECUTED')->sum('amount');
-//
-//        info($yesterdayTotalAmount);
-//        info($todayTotalAmount);
-
-
-//        $yesterdayStart = now()->setTimezone('UTC')->startOfDay();
-//        $yesterdayEnd = now()->setTimezone('UTC')->endOfDay();
-
-//        info($yesterdayStart);
-//        info($yesterdayEnd);
-
-//        $todayStart = Carbon::yesterday('UTC')->startOfDay();
-//        $todayEnd = Carbon::today('Asia/Manila')->endOfDay()->setTimezone('UTC');
-
-//        $totalAmountYesterday = SwiftpayQueryOrder::whereBetween('created_at', [$yesterdayStart, $yesterdayEnd])->sum('amount');
-
-//        $totalAmountToday = SwiftpayQueryOrder::whereBetween('created_at', [$todayStart, $todayEnd])->where('ORDER_STATUS', 'EXECUTED')->sum('amount');
-
-
-//        info(Carbon::yesterday('UTC')->startOfDay());
-//        info($totalAmountToday);
-
         $yesterdayStart = \Carbon\Carbon::yesterday('UTC')->startOfDay();
         $yesterdayEnd = \Carbon\Carbon::yesterday('UTC')->endOfDay();
         $totalAmountYesterday = SwiftpayQueryOrder::whereBetween('created_at', [$yesterdayStart, $yesterdayEnd])->where('ORDER_STATUS', 'EXECUTED')->sum('amount');
