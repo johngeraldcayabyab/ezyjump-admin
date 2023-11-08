8838a592151762fdb8371f455227065a9918821b
Dlareg <johngeraldcayabyab@gmail.com>
2023-11-08 17:27:13 +0800

    add active tabs

--- resources/views/components/transactions-tab.blade.php
+++ resources/views/components/transactions-tab.blade.php
@@ -2,12 +2,12 @@
     <ul class="text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
         <li class="w-full">
             <a href="{{route('transactions.swiftpay.show')}}"
-               class="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white"
+               class="inline-block w-full p-4 rounded-l-lg focus:ring-4 focus:ring-blue-300 focus:outline-none {{ request()->routeIs('transactions.swiftpay.show') ? 'text-gray-900 bg-gray-100  active dark:bg-gray-700 dark:text-white' : 'focus:outline-none bg-white hover:text-gray-700 hover:bg-gray-50  dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700' }}"
                aria-current="page">Swiftpay</a>
         </li>
         <li class="w-full">
             <a href="{{route('transactions.gcash.show')}}"
-               class="inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Gcash</a>
+               class="inline-block w-full p-4 rounded-l-lg focus:ring-4 focus:ring-blue-300 focus:outline-none {{ request()->routeIs('transactions.gcash.show') ? 'text-gray-900 bg-gray-100  active dark:bg-gray-700 dark:text-white' : 'focus:outline-none bg-white hover:text-gray-700 hover:bg-gray-50  dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700' }}">Gcash</a>
         </li>
     </ul>
 </div>
