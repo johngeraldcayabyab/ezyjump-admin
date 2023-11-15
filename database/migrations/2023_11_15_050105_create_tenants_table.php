75c954252297f2b1240008383a7b65ad4d23d9d6
Dlareg <johngeraldcayabyab@gmail.com>
2023-11-15 13:49:51 +0800

    add primary key and timestamp

--- database/migrations/2023_11_15_050105_create_tenants_table.php
+++ database/migrations/2023_11_15_050105_create_tenants_table.php
@@ -8,11 +8,13 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('tenants', function (Blueprint $table) {
+            $table->id();
             $table->unsignedInteger('user_id');
             $table->string('tenant_id');
             $table->string('channel');
             $table->string('authorization_token');
             $table->string('credentials')->nullable();
+            $table->timestamps();
         });
     }
 
