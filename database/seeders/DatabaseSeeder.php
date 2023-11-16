f68739a181ff2889d88034dbc9df4a7247bb0da7
Dlareg <johngeraldcayabyab@gmail.com>
2023-11-16 14:53:29 +0800

    remove tenant id in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Ezyjump Admin',
             'email' => 'ezyjumpitsolutions@gmail.com',
             'password' => Hash::make('admin'),
-            'tenant_id' => 'admin',
         ];
         if (app()->isProduction()) {
             $user['password'] = Hash::make('nuvdi2-rygbiv-fYvnit');
