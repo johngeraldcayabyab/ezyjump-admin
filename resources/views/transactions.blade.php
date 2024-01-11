f1ad997eee9c2eba34c818948e714bffa500c2cf
Dlareg <johngeraldcayabyab@gmail.com>
2024-01-11 19:12:43 +0800

    allow all transactions for now

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -2,16 +2,18 @@
     <x-slot name="header">
         <x-page-header title="{{ __('Transactions') }}"/>
     </x-slot>
-    @if(auth()->user()->isAdmin())
-        <x-transactions-tab></x-transactions-tab>
-        <x-swiftpay-orders></x-swiftpay-orders>
-    @else
-        @if(auth()->user()->channel === 'swiftpay')
-            <x-swiftpay-orders></x-swiftpay-orders>
-        @elseif(auth()->user()->channel === 'swiftpay-qr')
-            <x-swiftpay-qr-orders></x-swiftpay-qr-orders>
-        @endif
-    @endif
+    <x-transactions-tab></x-transactions-tab>
+    <x-swiftpay-orders></x-swiftpay-orders>
+{{--    @if(auth()->user()->isAdmin())--}}
+{{--        <x-transactions-tab></x-transactions-tab>--}}
+{{--        <x-swiftpay-orders></x-swiftpay-orders>--}}
+{{--    @else--}}
+{{--        @if(auth()->user()->channel === 'swiftpay')--}}
+{{--            <x-swiftpay-orders></x-swiftpay-orders>--}}
+{{--        @elseif(auth()->user()->channel === 'swiftpay-qr')--}}
+{{--            <x-swiftpay-qr-orders></x-swiftpay-qr-orders>--}}
+{{--        @endif--}}
+{{--    @endif--}}
 </x-app-layout>
 
 
