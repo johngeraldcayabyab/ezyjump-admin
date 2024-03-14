3d5a082ca69e510f00b4c728449a4ed7b39080fa
Dlareg <johngeraldcayabyab@gmail.com>
2024-03-15 02:44:18 +0800

    merchant data now viewable

--- resources/views/components/swiftpay-orders.blade.php
+++ resources/views/components/swiftpay-orders.blade.php
@@ -112,7 +112,7 @@
                             </button>
                             @if(auth()->user()->type === 'admin')
                                 <button
-                                    x-on:click="fetchDetails(order.id)"
+                                    x-on:click="await fetchDetails(order.id)"
                                     type="button"
                                     class="px-3 py-2 text-xs font-medium text-center text-white bg-indigo-600 rounded border-indigo-600 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
                                 >View
@@ -124,6 +124,51 @@
             </template>
         </x-slot:body>
     </x-table>
+</div>
+
+
+<div class="text-center hidden fixed">
+    <button
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        type="button" data-drawer-target="drawer-right-example" data-drawer-show="drawer-right-example"
+        data-drawer-placement="right" aria-controls="drawer-right-example">
+        Show right drawer
+    </button>
+</div>
+
+
+<div id="drawer-right-example"
+     class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-80 dark:bg-gray-800"
+     tabindex="-1" aria-labelledby="drawer-right-label">
+    <h5 id="drawer-right-label"
+        class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">
+        <svg class="w-4 h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+             viewBox="0 0 20 20">
+            <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        Order Information
+    </h5>
+    <button type="button" data-drawer-hide="drawer-right-example" aria-controls="drawer-right-example"
+            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white">
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+        </svg>
+        <span class="sr-only">Close menu</span>
+    </button>
+    <div id="drawer-content" class="mb-6 text-sm text-gray-500 dark:text-gray-400">
+        <div class="grid grid-cols-2 gap-4">
+            <div class="col-span-2">
+                <div class="text-gray-500 dark:text-gray-400 font-medium"><b>Tenant ID:</b></div>
+                <div id="tenant_id">${merchant.id}</div>
+            </div>
+            <div class="col-span-2">
+                <div class="text-gray-500 dark:text-gray-400 font-medium"><b>Account Name:</b></div>
+                <div id="account_name">${merchant.name}</div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <x-slot:script>
@@ -189,12 +234,17 @@
                 });
         }
 
-        function fetchDetails(id) {
+        async function fetchDetails(id) {
             let url = "{{ route('swiftpay_query_orders.show', ':id') }}";
             url = url.replace(':id', id);
-            return fetch(url)
+            fetch(url)
                 .then(response => response.json())
-                .then(response => response);
+                .then(json => {
+                    const merchant = json.data.merchant;
+                    document.querySelector('#tenant_id').innerHTML = merchant.id;
+                    document.querySelector('#account_name').innerHTML = merchant.preferred_account;
+                    document.querySelector('[data-drawer-show="drawer-right-example"]').click();
+                });
         }
     </script>
 </x-slot:script>
