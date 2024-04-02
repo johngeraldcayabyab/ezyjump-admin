3cb51cbe5ac1ecdb459ee82aac6b140c97e720b1
Dlareg <johngeraldcayabyab@gmail.com>
2024-04-02 17:52:49 +0800

    use authy instead of auth guard

--- resources/views/layouts/gateway-navigation.blade.php
+++ resources/views/layouts/gateway-navigation.blade.php
@@ -102,7 +102,7 @@
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Authy::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::guard('web')->user()->email }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Authy::user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
