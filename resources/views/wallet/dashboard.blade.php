0904812b4a861b058411461ad2dfb582c05f2876
Dlareg <johngeraldcayabyab@gmail.com>
2024-04-04 02:39:30 +0800

    add copy to clipboard

--- resources/views/wallet/dashboard.blade.php
+++ resources/views/wallet/dashboard.blade.php
@@ -17,21 +17,49 @@
                                 <div class="col-span-2">
                                     <div class="text-gray-500 dark:text-gray-300 font-medium"><b>MERCHANT NAME:</b>
                                     </div>
-                                    <div class="bg-slate-100 rounded p-1">{{Auth::user()->name}}</div>
+                                    <div class="bg-slate-100 rounded p-2">{{Auth::user()->name}}</div>
                                 </div>
                                 <div class="col-span-2">
                                     <div class="text-gray-500 dark:text-gray-300 font-medium"><b>API KEY:</b></div>
-                                    <div class="bg-slate-100 rounded p-1">{{Auth::user()->merchantKey->api_key}}</div>
+                                    <div class="bg-slate-100 rounded p-2 relative">
+                                        {{Auth::user()->merchantKey->api_key}}
+                                        <button class="absolute top-0 right-0 z-10 m-1" onclick="copyToClipboard('{{Auth::user()->merchantKey->api_key}}')">
+                                            <svg class="w-6 h-6 text-gray-400 dark:text-white" aria-hidden="true"
+                                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                                 viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linejoin="round" stroke-width="2"
+                                                      d="M14 4v3a1 1 0 0 1-1 1h-3m4 10v1a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2m11-3v10a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1V7.87a1 1 0 0 1 .24-.65l2.46-2.87a1 1 0 0 1 .76-.35H18a1 1 0 0 1 1 1Z"/>
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
                                 <div class="col-span-2">
                                     <div class="text-gray-500 dark:text-gray-300 font-medium"><b>SECRET KEY:</b></div>
-                                    <div
-                                        class="bg-slate-100 rounded p-1">{{Auth::user()->merchantKey->secret_key}}</div>
+                                    <div class="bg-slate-100 rounded p-2 relative">
+                                        {{Auth::user()->merchantKey->secret_key}}
+                                        <button class="absolute top-0 right-0 z-10 m-1" onclick="copyToClipboard('{{Auth::user()->merchantKey->secret_key}}')">
+                                            <svg class="w-6 h-6 text-gray-400 dark:text-white" aria-hidden="true"
+                                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                                 viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linejoin="round" stroke-width="2"
+                                                      d="M14 4v3a1 1 0 0 1-1 1h-3m4 10v1a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2m11-3v10a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1V7.87a1 1 0 0 1 .24-.65l2.46-2.87a1 1 0 0 1 .76-.35H18a1 1 0 0 1 1 1Z"/>
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
                                 <div class="col-span-2">
                                     <div class="text-gray-500 dark:text-gray-300 font-medium"><b>MERCHANT ID:</b></div>
-                                    <div
-                                        class="bg-slate-100 rounded p-1">{{Auth::user()->merchantKey->merchant_id}}</div>
+                                    <div class="bg-slate-100 rounded p-2 relative">
+                                        {{Auth::user()->merchantKey->merchant_id}}
+                                        <button class="absolute top-0 right-0 z-10 m-1" onclick="copyToClipboard('{{Auth::user()->merchantKey->merchant_id}}')">
+                                            <svg class="w-6 h-6 text-gray-400 dark:text-white" aria-hidden="true"
+                                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                                 viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linejoin="round" stroke-width="2"
+                                                      d="M14 4v3a1 1 0 0 1-1 1h-3m4 10v1a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2m11-3v10a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1V7.87a1 1 0 0 1 .24-.65l2.46-2.87a1 1 0 0 1 .76-.35H18a1 1 0 0 1 1 1Z"/>
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -42,6 +70,20 @@
 
         </div>
     </div>
+
+    <x-slot:script>
+        <script>
+            function copyToClipboard(text) {
+                const el = document.createElement('textarea');
+                el.value = text;
+                document.body.appendChild(el);
+                el.select();
+                document.execCommand('copy');
+                document.body.removeChild(el);
+                alert('Copied to clipboard');
+            }
+        </script>
+    </x-slot:script>
 </x-wallet-app-layout>
 
 
