82888aaace07dc38392462e8c3dc5e3159de90af
Dlareg <johngeraldcayabyab@gmail.com>
2024-04-04 01:34:03 +0800

    convert connection to tls

--- config/database.php
+++ config/database.php
@@ -209,6 +209,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
@@ -218,6 +219,7 @@ return [
         ],
 
         'cache' => [
+            'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
