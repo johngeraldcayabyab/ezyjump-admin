beb5f529b4692d7b1bb4ec7ca54edeef47bcaaae
Dlareg <johngeraldcayabyab@gmail.com>
2024-04-16 18:13:02 +0800

    log call

--- app/Http/Controllers/CallbackReceiverController.php
+++ app/Http/Controllers/CallbackReceiverController.php
@@ -9,6 +9,9 @@ class CallbackReceiverController
     public function receiver(Request $request)
     {
         $all = $request->all();
+        info('**This is a test receiver**');
+        info($all);
+        info('**This is a test receiver end**');
         return response(['status' => 200, 'content' => $all]);
     }
 }
