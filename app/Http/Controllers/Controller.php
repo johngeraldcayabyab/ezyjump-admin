b4109e17d8369eac36414c3ed71a59c3e9f0b956
Dlareg <johngeraldcayabyab@gmail.com>
2024-04-25 00:44:44 +0800

    remove comment in controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,7 +13,6 @@ class Controller extends BaseController
 
     public function getIn($model, ?string $field, mixed $value)
     {
-//        this is a comment 2
         $value = Str::replace(' ', '', $value);
         if (strlen($value)) {
             if (Str::contains($value, ',')) {
