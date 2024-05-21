e099b431170e3af85fc9b35b39314b04ed5a3ee3
Dlareg <johngeraldcayabyab@gmail.com>
2024-05-21 20:18:50 +0800

    asd

--- routes/api.php
+++ routes/api.php
@@ -38,5 +38,5 @@ Route::prefix('wallet')->group(function () {
 
 Route::get('magpie/pass-through', [CallbackReceiverController::class, 'magpie'])->name('magpie.pass-through');
 Route::post('magpie/callback', [CallbackReceiverController::class, 'postback'])->name('magpie.callback');
-Route::post('magpie/sync', [CallbackReceiverController::class, 'sync'])->name('magpie.sync');
+Route::get('magpie/sync', [CallbackReceiverController::class, 'sync'])->name('magpie.sync');
 Route::post('callback/receiver', [CallbackReceiverController::class, 'receiver'])->name('callbacks.receiver');
