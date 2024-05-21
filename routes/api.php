fe46fc7138c5b61e3032dc87f8c0669869b8c6d0
Dlareg <johngeraldcayabyab@gmail.com>
2024-05-21 20:00:54 +0800

    fix incorrect request

--- routes/api.php
+++ routes/api.php
@@ -37,5 +37,5 @@ Route::prefix('wallet')->group(function () {
 });
 
 Route::get('magpie/pass-through', [CallbackReceiverController::class, 'magpie'])->name('magpie.pass-through');
-Route::get('magpie/postback', [CallbackReceiverController::class, 'postback'])->name('magpie.postback');
+Route::post('magpie/postback', [CallbackReceiverController::class, 'postback'])->name('magpie.postback');
 Route::post('callback/receiver', [CallbackReceiverController::class, 'receiver'])->name('callbacks.receiver');
