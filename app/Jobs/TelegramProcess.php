908285b5e011b9bcae2122f531738e632177348d
Dlareg <johngeraldcayabyab@gmail.com>
2024-05-23 01:09:49 +0800

    added a telegram message to GC

--- app/Jobs/TelegramProcess.php
+++ app/Jobs/TelegramProcess.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class TelegramProcess implements ShouldQueue
@@ -33,12 +34,10 @@ class TelegramProcess implements ShouldQueue
         }
         $text = trim($message['text']);
         $this->log($text);
-
         if (strlen($text) > 200) {
             $this->log("Too long $text");
             return;
         }
-
         $magpieDeposit = WalletMagpieDeposit::where('transaction_id', $text)->first();
         if (!$magpieDeposit) {
             $this->log("$text is not a magpie deposit");
@@ -46,10 +45,28 @@ class TelegramProcess implements ShouldQueue
         }
         if ($magpieDeposit->status !== 'PENDING') {
             $this->log("$text is not pending! it's {$magpieDeposit->status}");
+            $this->sendMessage("$text status is still {$magpieDeposit->status} wait for PENDING then try again :)");
             return;
         }
         $this->log("Dispatching force pay through bot {$magpieDeposit->id}");
         MagpieForcePay::dispatch($magpieDeposit->id);
+        $this->sendMessage("$text has been synced :)");
+    }
+
+    private function sendMessage($message)
+    {
+        $token = config('tokens.TELEGRAM_TOKEN');
+        $chatId = '-4051086307';
+        $url = "https://api.telegram.org/bot{$token}/sendMessage";
+        $response = Http::post($url, [
+            'chat_id' => $chatId,
+            'text' => $message,
+        ]);
+        if ($response->successful()) {
+            $this->log('Message sent successfully!');
+        } else {
+            $this->log('Failed to send message: ' . $response->body());
+        }
     }
 
     private function log($message)
--- config/tokens.php
+++ config/tokens.php
@@ -2,4 +2,5 @@
 
 return [
     'EZYJUMP_TOKEN' => env('EZYJUMP_TOKEN'),
+    'TELEGRAM_TOKEN' => env('TELEGRAM_TOKEN'),
 ];
